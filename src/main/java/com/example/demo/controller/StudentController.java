3fd3fd3e3bd047ba29ada1dcaf59d39a620a225a
dnuvsin <92454497+dnuvsin@users.noreply.github.com>
2023-08-29 14:01:19 +0700

    Update StudentController.java

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Student;
-import com.example.demo.model.Student;
 import com.example.demo.repository.StudentRepository;
 
 @RestController
